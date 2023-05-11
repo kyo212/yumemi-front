cd539c9e96dc1c2aaf7e358ae99ed87a818f9430
kyo212 <kyohy163@gmail.com>
2023-05-11 16:20:48 +0900

    feat: 取得した都道府県を全てcheckboxで表示させる記述を追加

--- src/components/Prefectures.vue
+++ src/components/Prefectures.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import axios from "axios";
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 
-onMounted(() => {
-  axios
-    .get(import.meta.env.VITE_APP_RESAS_URL, {
-      headers: { "X-API-KEY": import.meta.env.VITE_APP_API_KEY },
-    })
-    .then(({ data }) => {
-      console.log(data.result);
-    });
+const prefectures = ref();
+
+onMounted(async () => {
+  const getPrefectureList = await axios.get(import.meta.env.VITE_APP_RESAS_URL, {
+    headers: { "X-API-KEY": import.meta.env.VITE_APP_API_KEY },
+  });
+  prefectures.value = getPrefectureList.data.result;
 });
 </script>
 
 <template>
   <h1>都道府県</h1>
+  <div v-for="prefecture in prefectures">
+    <label>{{ prefecture.prefName }}</label>
+    <input type="checkbox" />
+  </div>
 </template>
