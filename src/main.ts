b5de783534414dda8134facc55c4a57b3c188526
kyo212 <kyohy163@gmail.com>
2023-05-11 15:33:10 +0900

    refactor: 必要のない記述を削除

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
-
-VITE_APP_RESAS_URL=https://opendata.resas-portal.go.jp/api/v1/prefectures
-VITE_APP_API_KEY=HG2a7QXLK670jtFcwbDGH6309o9D6wzPW74MoS8X
+createApp(App).mount("#app");
