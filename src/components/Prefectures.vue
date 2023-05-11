e1a8d0557fe721fe609404163b92447af107f8ad
kyo212 <kyohy163@gmail.com>
2023-05-11 17:10:06 +0900

    refactor: いらない改行の削除

--- src/components/Prefectures.vue
+++ src/components/Prefectures.vue
@@ -5,11 +5,9 @@ import { ref, onMounted } from "vue";
 const prefectures = ref();
 
 onMounted(async () => {
-  
   const getPrefectureList = await axios.get(import.meta.env.VITE_APP_RESAS_URL,{
     headers: { "X-API-KEY": import.meta.env.VITE_APP_API_KEY },
   });
-  
   prefectures.value = getPrefectureList.data.result;
 });
 </script>
