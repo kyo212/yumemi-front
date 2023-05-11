e099f71338a3d5144e0c1fd05a1623e4590734b8
kyo212 <kyohy163@gmail.com>
2023-05-11 16:55:37 +0900

    fix: 更新

--- src/components/Prefectures.vue
+++ src/components/Prefectures.vue
@@ -5,9 +5,11 @@ import { ref, onMounted } from "vue";
 const prefectures = ref();
 
 onMounted(async () => {
+  
   const getPrefectureList = await axios.get(import.meta.env.VITE_APP_RESAS_URL, {
     headers: { "X-API-KEY": import.meta.env.VITE_APP_API_KEY },
   });
+  
   prefectures.value = getPrefectureList.data.result;
 });
 </script>
