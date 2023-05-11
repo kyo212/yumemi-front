9aaa3c57186c4d5238c51940d1047a2b366af245
kyo212 <kyohy163@gmail.com>
2023-05-11 15:17:36 +0900

    chore: .envファイルをリモートリポジトリから削除するため、ローカルで削除

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,6 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+VITE_APP_RESAS_URL=https://opendata.resas-portal.go.jp/api/v1/prefectures
+VITE_APP_API_KEY=HG2a7QXLK670jtFcwbDGH6309o9D6wzPW74MoS8X
