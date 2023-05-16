6a0958bf764214c0a2d63dd755328afdb2cd05a3
kyo212 <kyohy163@gmail.com>
2023-05-16 21:59:18 +0900

    refactor: 必要ないコメントと記述の削除

--- src/components/PrefecturesInfo.vue
+++ src/components/PrefecturesInfo.vue
@@ -4,52 +4,44 @@ import { SelectedPrefectures, SelectedPopulation } from "../types/index";
 import { getPrefectures, getPopulation } from "../api/resas";
 
 const prefecturesList = ref<SelectedPrefectures[]>([]);
-const selectedPrefectures = ref<SelectedPrefectures[]>([]); // APIからのレスポンスに基づく値を持つ
-const selectedPopulation = ref<SelectedPopulation[]>([]); // ユーザーの選択に基づく値を持つ
+const selectedPrefectures = ref<SelectedPrefectures[]>([]);
+const selectedPopulation = ref<SelectedPopulation[]>([]);
 
 // 都道府県のリストを取得する
-// 都道府県コード0~46, 都道府県名
 onMounted(async () => {
   const response = await getPrefectures();
   prefecturesList.value = response;
 });
 
-// 取得した都道府県情報の中の都道府県コードを利用して、その都道府県の人口を取得する機能
-// 境界年度、（総人口、年少人口、生産年齢人口、老年人口）
+// 都道府県コードをもとに、その都道府県の人口を取得する機能
 watch(selectedPrefectures, async (newValues, oldValues) => {
-  // 現在チェックされた値のみでAPIを叩くための絞り込み処理
+  // 最も新しくチェックした要素の特定
   const newSelectedPrefecture = newValues.filter(
     (newValue) =>
       !oldValues.find((oldValue) => oldValue.prefCode === newValue.prefCode)
   );
-
-  // チェックされた都道府県のコード（prefCode）に基づいてAPIを叩く処理。checkが外された場合、下記のコードは実行されない
-  if (newSelectedPrefecture.length) {
-    const response = await getPopulation(newSelectedPrefecture[0]);
+  const selected = newSelectedPrefecture[0];
+  if (selected) {
+    const response = await getPopulation(selected);
     selectedPopulation.value.push({
-      prefCode: newSelectedPrefecture[0].prefCode,
+      prefCode: selected.prefCode,
       data: response.data,
     });
   } else {
-    // チェックが外された要素の特定。oldValues（現在チェック中の要素の１つ前の状態）の中からnewValuesに含まれない要素を抽出
+    // チェックが外された要素の特定
     const removedSelection = oldValues.find(
       (oldValue) => !newValues.includes(oldValue)
     );
-    // チェックが外れた要素を人口構成リストから削除
-    // チェックが外れた要素のインデックスの特定→インデックスを基に削除
-    if (removedSelection) {
-      const index = selectedPopulation.value.findIndex(
-        (population) => population.prefCode === removedSelection.prefCode
-      );
-      selectedPopulation.value.splice(index, 1);
-    }
+    // チェックが外された要素のインデックスを特定
+    const removeIndex = selectedPopulation.value.findIndex(
+      (population) => population.prefCode === removedSelection?.prefCode
+    );
+    selectedPopulation.value.splice(removeIndex, 1);
   }
 });
 </script>
 
 <template>
-  {{ selectedPrefectures }}
-  {{ selectedPopulation }}
   <h1>都道府県</h1>
   <ul class="grid grid-cols-4">
     <li
