608d9aa3bb814deccb70c2eb19339dd94214b22a
kyo212 <kyohy163@gmail.com>
2023-05-16 15:25:56 +0900

    refactor: タイプの記述をコンポーネント化

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type SelectedPrefectures = {
+  prefCode: number;
+  prefName: string;
+};
+
+export type SelectedPopulation = {
+  prefCode: number;
+  label: string;
+  data: any;
+};
---
+++ src/types/vue3-highcharts.d.ts
@@ -0,0 +1,4 @@
+declare module 'vue3-highcharts' {
+  const Vue3Highcharts: any;
+  export default Vue3Highcharts;
+}
