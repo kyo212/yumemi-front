123f1529d5aa408378d825fce80281368b0152ce
kyo212 <kyohy163@gmail.com>
2023-05-23 18:05:36 +0900

    feat: 選択した都道府県の情報を表示

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,47 +1,39 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import VueHighcharts from "vue3-highcharts";
+import { ref, watchEffect } from "vue";
+import { usePrefectureStore } from "../stores/prefectureStore";
 
-const chartOptions = computed(() => ({
-  chart: {
-    type: "line",
-  },
-  title: {
-    text: "都道府県別の人口",
-  },
-  xAxis: {
-    categories: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-  },
-  series: [
-    {
-      name: "Tokyo",
-      data: [
-        7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,
-      ],
-    },
-    {
-      name: "New York",
-      data: [
-        -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5,
-      ],
-    },
-  ],
-}));
+const store = usePrefectureStore();
+
+const populationData = ref<any>([]);
+
+watchEffect(() => {
+  populationData.value = store.selectedPopulation;
+});
 </script>
 
 <template>
-  <vue-highcharts type="chart" :options="chartOptions" />
+  <ul>
+    <li v-for="data in populationData">
+      <h3 class="bold text-2xl text-red-400">{{ data.name }}</h3>
+      <table v-for="num in data.data" class="w-full bg-slate-100">
+        <thead>
+          <tr>
+            <th>{{ num.label }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td v-for="n in num.data">
+              {{ n.year }}
+            </td>
+          </tr>
+          <tr>
+            <td v-for="n in num.data">
+              {{ n.value }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </li>
+  </ul>
 </template>
